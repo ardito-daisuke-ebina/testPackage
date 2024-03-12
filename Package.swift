db0105a5b0a9fcf3d9a8c10d7383c25b3a3cef24
daisuke.ebina <daisuke.ebina@ardito.co.jp>
2024-03-12 13:49:01 +0900

    update package

--- Package.swift
+++ Package.swift
@@ -16,8 +16,5 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "Ebinaframework",
                       path: "Ebinaframework.xcframework"),
-        .testTarget(
-            name: "testPackageTests",
-            dependencies: ["testPackage"]),
     ]
 )
